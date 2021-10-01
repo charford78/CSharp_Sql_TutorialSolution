0184ca3ff0fbfba0e2bf9f3e9a87dbac2603c770
Charlie Ford <charles.ford1978@gmail.com>
2021-10-01 16:06:06 -0400

    Add project files.

---
+++ CSharp_Sql_Tutorial/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+
+namespace CSharp_Sql_Tutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connStr = "server=localhost\\sqlexpress;database=PrsDb;" +
+                "trusted_connection=true;";
+            var sqlConn = new SqlConnection(connStr);
+            sqlConn.Open();
+            if(sqlConn.State != System.Data.ConnectionState.Open)
+            {
+                Console.WriteLine("Connection did not open");
+                return;
+            }
+            Console.WriteLine("Connection opened.");
+
+            // continue sql here
+
+            sqlConn.Close();
+        }
+    }
+}
