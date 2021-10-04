84a47167cc9cb3f4188004c7dc101b590c16cbc8
Charlie Ford <charles.ford1978@gmail.com>
2021-10-04 11:02:56 -0400

    Added variables to read from SQL table Student in EdDb

--- CSharp_Sql_Tutorial/Program.cs
+++ CSharp_Sql_Tutorial/Program.cs
@@ -7,7 +7,7 @@ namespace CSharp_Sql_Tutorial
     {
         static void Main(string[] args)
         {
-            var connStr = "server=localhost\\sqlexpress;database=PrsDb;" +
+            var connStr = "server=localhost\\sqlexpress;database=EdDb;" +
                 "trusted_connection=true;";
             var sqlConn = new SqlConnection(connStr);
             sqlConn.Open();
@@ -19,6 +19,27 @@ namespace CSharp_Sql_Tutorial
             Console.WriteLine("Connection opened.");
 
             // continue sql here
+            var sql = "Select * from Student;";
+            var cmd = new SqlCommand(sql, sqlConn);
+            var reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                var Id = Convert.ToInt32(reader["Id"]);
+                var FirstName = reader["Firstname"].ToString();
+                var LastName = Convert.ToString(reader["Lastname"]);
+                var StateCode = reader["StateCode"].ToString();
+                var SAT = reader["SAT"].Equals(DBNull.Value) //ternary statement to check for nulls since SAT column in SQL allows nulls
+                    ? (int?)null//if the statment above is true do this.
+                    : Convert.ToInt32(reader["SAT"]);//if false do this.
+                var GPA = Convert.ToDecimal(reader["GPA"]);
+                //using the ternary statement again for the SAT variable.
+                var message = $"{Id} | {FirstName} {LastName} | {StateCode} | {(SAT != null ? SAT : "NULL")}" +
+                    $" | {GPA}";
+                Console.WriteLine($"{ message}");
+            }
+
+            //best practice is to enter this reader close statment before you start the body of the while loop.
+            reader.Close();
 
             sqlConn.Close();
         }
---
+++ CSharp_Sql_Tutorial/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Sql_Tutorial
+{
+    class Student
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string StateCode { get; set; }
+        public int? SAT { get; set; }
+        public decimal GPA { get; set; }
+        public int? MajorId { get; set; }
+    }
+}
